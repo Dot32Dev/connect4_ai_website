0b1d423e749e63db1443d6862b5c8be6a26aa733
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2024-10-31 15:51:33 +0800

    Attempting to fix deploy

--- index.js
+++ index.js
@@ -2,65 +2,166 @@ import init, { State } from "./pkg/connect4_ai_website.js";
 // init().then(() => {
 //   greet("WebAssembly");
 // });
+//
+
+// Download the shader
+// const response = await fetch("/generation_452_ai.ron");
+// const AIData = await response.text();
+
+const rows = 6;
+const columns = 7;
+
+let game;
+
+window.win = function () {
+  window.inactive = true;
+  window.won = true;
+
+  // const turn_text = document.querySelector(`span.${turn}`);
+  // turn_text.classList.remove(window.turn);
+  window.turn = window.turn === "red" ? "blue" : "red";
+  // turn_text.classList.add(window.turn);
+  console.log(`${window.turn} won`);
+  // const turn_text = document.querySelector(`span.${turn}`).parentElement;
+  // turn_text.innerHTML = `<span class="${window.turn}">${window.turn[0].toUpperCase() + window.turn.slice(1)}</span> won!`;
+
+  const turnText = document.querySelector("p"); // Select the p element directly
+  turnText.innerHTML = `<span class="${window.turn}">${window.turn[0].toUpperCase() + window.turn.slice(1)}</span> won!`;
+
+  // Create and insert reload message
+  const reloadMessage = document.createElement("p");
+  reloadMessage.innerHTML =
+    '<a href="javascript:location.reload()">Play again</a>';
+  reloadMessage.style.cursor = "pointer";
+  turnText.parentElement.append(reloadMessage);
+
+  document.body.classList.add("inactive");
+};
+
+window.ai_loaded_in_rust = function () {
+  window.ai_loaded = true;
+};
 
 init().then(() => {
-  const rows = 6;
-  const columns = 7;
+  window.turn = "red";
+  game = new State();
+  window.inactive = false;
+  window.won = false;
+  window.ai_loaded = false;
 
-  let turn = "red";
+  initialise_board();
+  loadAI();
+});
 
-  let game = new State();
+async function loadAI() {
+  try {
+    // const response = await fetch("/generation_452_ai.ron");
+    const response = await fetch("/generation_452_ai.json", {
+      headers: {
+        Accept: "text/plain", // Force text format
+      },
+    });
+    const AIData = await response.text();
+    console.log("Loaded AI data:", AIData.substring(0, 100)); // Log first 100 chars
+    game.set_ai(AIData);
+    console.log("AI loaded successfully");
+  } catch (error) {
+    console.error("Failed to load AI:", error);
+  }
+}
 
-  function initialise_board() {
-    const board = document.querySelector(".board");
+function initialise_board() {
+  const board = document.querySelector(".board");
 
-    for (let col = 1; col <= columns; col++) {
-      const column = document.createElement("div");
-      column.classList.add("column");
-      column.setAttribute("data-col", col);
+  for (let col = 1; col <= columns; col++) {
+    const column = document.createElement("div");
+    column.classList.add("column");
+    column.setAttribute("data-col", col);
 
-      column.addEventListener("click", (e) => {
-        let target_cell;
-        for (const cell of column.children) {
-          if (!cell.hasChildNodes()) {
-            target_cell = cell;
-            break;
-          }
+    column.addEventListener("click", (e) => {
+      let target_cell;
+      for (const cell of column.children) {
+        if (!cell.hasChildNodes()) {
+          target_cell = cell;
+          break;
         }
-
-        if (target_cell) {
-          const row = target_cell.getAttribute("data-row");
-          const piece = document.createElement("div");
-          piece.classList.add(turn);
-          piece.classList.add("piece");
-          piece.style.transform = `translateY(${-(50 * (rows + 0 - row) + 5)}px)`;
-          target_cell.appendChild(piece);
-          setTimeout(() => {
-            piece.style.transform = "translateY(0px)";
-          }, 10);
-
-          // greet(`${turn} placed a piece in row ${col}`);
-          game.place(col);
-
-          const turn_text = document.querySelector(`span.${turn}`);
-          turn_text.classList.remove(turn);
-          turn = turn === "red" ? "blue" : "red";
-          turn_text.classList.add(turn);
-          turn_text.innerHTML = `${turn[0].toUpperCase() + turn.slice(1)}'s`;
-        }
-      });
-
-      for (let row = 1; row <= rows; row++) {
-        const cell = document.createElement("div");
-        cell.classList.add("cell");
-        cell.setAttribute("data-row", row);
-
-        column.appendChild(cell); // Add cell to column
       }
 
-      board.appendChild(column); // Add column to board
+      if (target_cell && !inactive) {
+        const row = target_cell.getAttribute("data-row");
+        const piece = document.createElement("div");
+        piece.classList.add(turn);
+        piece.classList.add("piece");
+        piece.style.transform = `translateY(${-(50 * (rows + 0 - row) + 5)}px)`;
+        target_cell.appendChild(piece);
+        setTimeout(() => {
+          piece.style.transform = "translateY(0px)";
+        }, 10);
+
+        // greet(`${turn} placed a piece in row ${col}`);
+
+        const turn_text = document.querySelector(`span.${window.turn}`);
+        turn_text.classList.remove(turn);
+        window.turn = window.turn === "red" ? "blue" : "red";
+        turn_text.classList.add(turn);
+        turn_text.innerHTML = `${window.turn[0].toUpperCase() + window.turn.slice(1)}'s`;
+
+        game.place(col - 1);
+
+        if (!window.won) {
+          ai_turn();
+        }
+      }
+    });
+
+    for (let row = 1; row <= rows; row++) {
+      const cell = document.createElement("div");
+      cell.classList.add("cell");
+      cell.setAttribute("data-row", row);
+
+      column.appendChild(cell); // Add cell to column
+    }
+
+    board.appendChild(column); // Add column to board
+  }
+}
+
+function ai_turn() {
+  window.inactive = true;
+  document.body.classList.add("inactive");
+
+  let column = game.ai_place();
+
+  column += 1;
+  let target_column = document.querySelector(`[data-col="${column}"]`);
+  let target_cell;
+
+  for (const cell of target_column.children) {
+    if (!cell.hasChildNodes()) {
+      target_cell = cell;
+      break;
     }
   }
 
-  initialise_board();
-});
+  if (target_cell) {
+    const row = target_cell.getAttribute("data-row");
+    const piece = document.createElement("div");
+    piece.classList.add("blue");
+    piece.classList.add("piece");
+    piece.style.transform = `translateY(${-(50 * (rows + 0 - row) + 5)}px)`;
+    target_cell.appendChild(piece);
+    setTimeout(() => {
+      piece.style.transform = "translateY(0px)";
+    }, 10);
+
+    const turn_text = document.querySelector(`span.${window.turn}`);
+    turn_text.classList.remove(turn);
+    window.turn = window.turn === "red" ? "blue" : "red";
+    turn_text.classList.add(turn);
+    turn_text.innerHTML = `${window.turn[0].toUpperCase() + window.turn.slice(1)}'s`;
+  }
+  if (!window.won) {
+    window.inactive = false;
+    document.body.classList.remove("inactive");
+  }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,24 @@
 use connect4_board_library::Bitboard;
+use nalgebra::{DMatrix, DVector};
+use serde::{Deserialize, Serialize};
+// use std::any::type_name;
+// use std::any::TypeId;
 use wasm_bindgen::prelude::*;
 
 // JS functions that rust can call
 #[wasm_bindgen]
 extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
     pub fn alert(s: &str);
+    pub fn win();
+    pub fn ai_loaded_in_rust();
 }
-
-// Rust function that JS can call
-// #[wasm_bindgen]
-// pub fn greet(name: &str) {
-//     alert(&format!("Hello, {}!", name));
-// }
 
 #[wasm_bindgen]
 pub struct State {
     bitboard: Bitboard,
+    ai: Option<NeuralNetwork>,
 }
 
 #[wasm_bindgen]
@@ -23,17 +26,236 @@ impl State {
     #[wasm_bindgen(constructor)]
     pub fn new() -> Self {
         let bitboard = Bitboard::new();
-        return Self { bitboard };
+        return Self { bitboard, ai: None };
     }
 
     pub fn place(&mut self, column: usize) {
         self.bitboard.drop_piece(column);
 
+        log(&format!("{}", self.bitboard));
+
         if self.bitboard.check_win() {
-            alert(&format!(
-                "Player {} won",
-                (self.bitboard.move_counter & 1) + 1
-            ));
+            // alert(&format!(
+            //     "Player {} won",
+            //     (self.bitboard.move_counter & 0) + 1
+            // ));
+            win();
+        }
+    }
+
+    pub fn set_ai(&mut self, data: &str) {
+        // let ai: NeuralNetwork =
+        //     ron::from_str(data).expect("Failed to deserialize ai");
+        // self.ai = Some(ai);
+
+        log(&format!("Attempting to parse AI data: {}", &data[..100]));
+
+        // match ron::from_str(data) {
+        //     Ok(ai) => {
+        //         self.ai = Some(ai);
+        //         log(&"AI successfully parsed and set");
+        //     }
+        //     Err(e) => {
+        //         log(&format!("Failed to parse AI data: {}", e));
+        //         panic!("Failed to parse AI data: {}", e);
+        //     }
+        // }
+        // Change from ron::from_str to serde_json::from_str
+        match serde_json::from_str(data) {
+            Ok(ai) => {
+                self.ai = Some(ai);
+                log("AI successfully parsed and set");
+                ai_loaded_in_rust();
+            }
+            Err(e) => {
+                log(&format!("Failed to parse AI data: {}", e));
+                panic!("Failed to parse AI data: {}", e);
+            }
+        }
+    }
+
+    pub fn ai_place(&mut self) -> usize {
+        // let ai = self.ai.unwrap();
+
+        // ai.make_move(
+        //     &mut self.bitboard,
+        //     self.bitboard.move_counter & 1,
+        //     1 - (self.bitboard.move_counter & 1),
+        // )
+
+        let move_counter = self.bitboard.move_counter;
+
+        if let Some(ai) = self.ai.as_ref() {
+            let column = ai.make_move(
+                &mut self.bitboard,
+                move_counter & 1,
+                1 - (move_counter & 1),
+            );
+
+            log(&format!("{}", self.bitboard));
+            log(&format!("ai placed in {}", column));
+
+            if self.bitboard.check_win() {
+                // alert(&format!(
+                //     "Player {} won",
+                //     (self.bitboard.move_counter & 0) + 1
+                // ));
+                win();
+            }
+
+            column
+        } else {
+            log("AI not initialised");
+            panic!("AI not initialised");
         }
     }
 }
+
+//----------------------------------------------------------------------------\\
+//----------------------------------------------------------------------------\\
+//----------------------------------------------------------------------------\\
+
+// #[typetag::serde(tag = "type")]
+trait Participant {
+    fn make_move(
+        &self,
+        game: &mut Bitboard,
+        you: usize,
+        opponent: usize,
+    ) -> usize;
+    // fn type_id(&self) -> TypeId;
+    // fn adjust(&mut self, range: f64);
+}
+
+#[derive(Serialize, Deserialize)]
+struct NeuralNetwork {
+    #[serde(rename = "type")]
+    type_name: String, // Add this to handle the "type" field in JSON
+    id: u8,
+    layers: Vec<DMatrix<f64>>, // Weights for each layer
+    biases: Vec<DVector<f64>>, // Biases for each layer
+}
+
+impl NeuralNetwork {
+    // fn new(layer_sizes: &[usize]) -> Self {
+    //     let mut layers = Vec::new();
+    //     let mut biases = Vec::new();
+
+    //     let mut rng = rand::thread_rng();
+    //     let uniform = Uniform::new(-1.0, 1.0);
+
+    //     for i in 0..layer_sizes.len() - 1 {
+    //         // // Randomly initialize weights and biases
+    //         // let layer =
+    //         //     DMatrix::<f64>::new_random(layer_sizes[i + 1], layer_sizes[i]);
+
+    //         // Randomly initialize weights and biases
+    //         let layer =
+    //             DMatrix::from_fn(layer_sizes[i + 1], layer_sizes[i], |_, _| {
+    //                 uniform.sample(&mut rng)
+    //             });
+
+    //         // let bias = DVector::<f64>::new_random(layer_sizes[i + 1]);
+
+    //         let bias = DVector::from_fn(layer_sizes[i + 1], |_, _| {
+    //             uniform.sample(&mut rng)
+    //         });
+    //         layers.push(layer);
+    //         biases.push(bias);
+    //     }
+
+    //     NeuralNetwork {
+    //         id: rng.gen::<u8>(),
+    //         layers,
+    //         biases,
+    //     }
+    // }
+
+    fn forward(&self, input: &DVector<f64>) -> DVector<f64> {
+        let mut output = input.clone();
+        for (layer, bias) in self.layers.iter().zip(self.biases.iter()) {
+            output = layer * output + bias; // Matrix multiplication and bias addition
+            output = output.map(|x| x.max(0.0)); // Apply ReLU activation
+        }
+        output
+    }
+}
+
+fn bitboard_to_input(player1: u64, player2: u64) -> DVector<f64> {
+    let mut input = DVector::zeros(42); // 42 playable positions (6x7 grid)
+
+    for i in 0..42 {
+        let mask = 1u64 << i;
+        if player1 & mask != 0 {
+            input[i] = -1.0;
+        } else if player2 & mask != 0 {
+            input[i] = 1.0;
+        }
+        // If neither player has a piece here, it remains 0.0
+    }
+
+    input
+}
+
+fn sort_indices_by_values(arr: &DVector<f64>) -> Vec<usize> {
+    let mut indices: Vec<usize> = (0..arr.len()).collect();
+
+    // Sort the indices based on the corresponding values in the DVector, in
+    // reverse for largest first
+    indices.sort_by(|&i, &j| arr[j].partial_cmp(&arr[i]).unwrap());
+
+    indices
+}
+
+// #[typetag::serde]
+impl Participant for NeuralNetwork {
+    fn make_move(
+        &self,
+        game: &mut Bitboard,
+        you: usize,
+        opponent: usize,
+    ) -> usize {
+        let input =
+            bitboard_to_input(game.players[you], game.players[opponent]);
+        let output = sort_indices_by_values(&self.forward(&input));
+        for i in 0..7 {
+            if game.drop_piece(output[i]) {
+                return output[i];
+            }
+        }
+        log("Found no columns with space");
+        return 0;
+    }
+
+    // fn type_id(&self) -> TypeId {
+    //     TypeId::of::<Self>()
+    // }
+
+    // fn adjust(&mut self, range: f64) {
+    //     for (layer, bias) in self.layers.iter_mut().zip(self.biases.iter_mut())
+    //     {
+    //         layer.apply(|w| *w += range * (2.0 * rand::random::<f64>() - 1.0));
+    //         bias.apply(|b| *b += range * (2.0 * rand::random::<f64>() - 1.0));
+    //     }
+    // }
+}
+
+// fn type_name_of<T>(_: T) -> &'static str {
+//     type_name::<T>()
+// }
+
+// impl fmt::Debug for NeuralNetwork {
+//     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+//         // All I really want it to print is the type name
+//         write!(
+//             f,
+//             "{}, id: {}",
+//             type_name_of(NeuralNetwork {
+//                 id: 0,
+//                 layers: Vec::new(),
+//                 biases: Vec::new()
+//             }),
+//             self.id
+//         )
+//     }
+// }
